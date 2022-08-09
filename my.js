1015645d603b84d319e9be4601b88e45e91fa05b
Yanzhidong <2223126487@qq.com>
2022-08-09 21:16:25 +0800

    2022.8。9日 更新 点击切换背景颜色，以及历史记录信息

--- my.js
+++ my.js
@@ -2,20 +2,32 @@ time = document.querySelector('h1')
 btn = document.querySelector('.icon-duigouxiao')
 text = document.querySelector('input')
 record = document.querySelector('.record')
+body = document.querySelector('body')
+
 
 let now = dayjs().format('YYYY-MM-DD hh:mm:ss')
 time.innerText = now
 setInterval(function(){
     let now = dayjs().format('YYYY-MM-DD hh:mm:ss')
-    text.innerText = now
+    time.innerText = now
 },100)
+
+text.onfocus=function(){
+    // body.style = "backgroundColor='white'"
+    body.style.background='white';
+    time.style.color = 'black';
+    text.style.borderBottom='5px solid black';
+    btn.style.color = 'black';
+    text.style.color = 'black';
+}
 /* 点击按钮 获取内容 展示每次记录内容 */
 let num = 1
 btn.onclick=function(){
-    let elem = document.createElement("div");
+    let elem = document.createElement("h3");
     if(num<=5){
         elem.innerText = text.value
-        elem.style = 'color:white;'
+        text.value = ''
+        elem.style = 'color:black;'
         record.appendChild(elem)
     }
     num+=1
